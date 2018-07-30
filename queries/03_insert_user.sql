fd3f3936f2f6a658ba6b55231bc1cc8ffd8bc6f6
David Stewart <davidstewy@gmail.com>
2018-07-30 16:59:55 -0400

    finished part 1 with queries

---
+++ queries/03_insert_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO users 
+(id, username, bio)
+VALUES (1, 'admin', 'I am a just a lonely administrator');
---
+++ queries/04_insert_posts.sql
@@ -0,0 +1,3 @@
+INSERT INTO posts
+VALUES (1, 'Admin announcement', 'This API is awesome!', 1),
+       (2, 'Just a test', 'This is just a test. I repeat, this is just.', 1);
